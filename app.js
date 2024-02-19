4f6e0b218a7af853dff6bf165fe2c2585db6d35c
Emmanuel Mensah <ninodonoyiti2@gmail.com>
2024-02-19 15:16:26 +0000

    Swipper added, footer added footer edited

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     const heartIcons = document.querySelectorAll(".heart");
-
+// heart
     heartIcons.forEach(icon => {
         icon.addEventListener("click", function() {
             this.classList.toggle("is-active");
         });
     });
-
+// filter
     var mixer = mixitup('.product-list', {
         selectors: {
             target: '.product-card'
@@ -15,4 +15,32 @@ document.addEventListener("DOMContentLoaded", function() {
             duration: 300
         }
     });
+    // swiper
+    var swiper = new Swiper(".mySwiper",{
+        loop: true,
+        autoplay: {
+            delay:2500,
+            disabledOnInteraction: false,
+        },
+        sliderPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        breakpoints: {
+            640:{
+                sliderPerView:2,
+                spaceBetween:20,
+            },
+            768:{
+                sliderPerView:3,
+                spaceBetween:40,
+            },
+            1024:{
+                sliderPerView:3,
+                spaceBetween:50,
+            },
+        }
+    })
 });
